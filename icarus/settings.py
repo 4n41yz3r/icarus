3d5402c7979b3e72a3f0b8e782dee7773352b17d
shellexecute@gmail.com <shellexecute@gmail.com>
2017-09-16 20:49:52 +0000

    Rename project hvc to hmc.

--- icarus/settings.py
+++ icarus/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'hvc.apps.HvcConfig',
+    'hmc.apps.HmcConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- icarus/urls.py
+++ icarus/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('hvc.urls')),
-    url(r'^hmc/', include('hvc.urls')),
+    url(r'^$', include('hmc.urls')),
+    url(r'^hmc/', include('hmc.urls')),
     url(r'^admin/', admin.site.urls),
 ]
